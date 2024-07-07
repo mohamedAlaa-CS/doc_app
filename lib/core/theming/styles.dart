ab5e5d7cf6975d3302b28588667f3cdb3e274a22
Myproject <mohamedAlaa-CS>
2024-07-07 23:21:33 +0300

    create spacing file , app text button widget, app text filed widget ,login ui

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -8,6 +8,11 @@ class TextStyles {
     fontWeight: FontWeight.w700,
     color: Colors.black,
   );
+  static TextStyle font24Blue700Weight = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.mainBlue,
+  );
   static TextStyle font32BlueBold = TextStyle(
     fontSize: 32.sp,
     fontWeight: FontWeight.bold,
@@ -18,9 +23,49 @@ class TextStyles {
     fontWeight: FontWeight.normal,
     color: AppColors.gray,
   );
+  static TextStyle font13DarkBlueWeight500 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.darkBlue,
+  );
+  static TextStyle font13DarkBlueWeight400 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.darkBlue,
+  );
+  static TextStyle font13BlueWeight600 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.mainBlue,
+  );
+  static TextStyle font13BlueRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.normal,
+    color: AppColors.mainBlue,
+  );
   static TextStyle font16WhiteSemibold = TextStyle(
     fontSize: 16.sp,
     fontWeight: FontWeight.w500,
     color: Colors.white,
+  );
+  static TextStyle font16WhiteWeight600 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+  static TextStyle font14GrayWheight400 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.gray,
+  );
+  static TextStyle font14LightGrayWheight500 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.lightGray,
+  );
+  static TextStyle font14DartBlueWheight500 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.darkBlue,
   );
 }
---
+++ lib/core/widgets/app_text_form_field.dart
@@ -0,0 +1,62 @@
+import 'package:doc_app/core/theming/colors.dart';
+import 'package:doc_app/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextFormField extends StatelessWidget {
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? focusedBorder;
+  final InputBorder? enabledBorder;
+  final TextStyle? inputTextStyle;
+  final TextStyle? hintStyle;
+  final String hintText;
+  final bool? isObscureText;
+  final Widget? suffixIcon;
+  final Color? backgroundColor;
+  const AppTextFormField({
+    super.key,
+    this.contentPadding,
+    this.focusedBorder,
+    this.enabledBorder,
+    this.inputTextStyle,
+    this.hintStyle,
+    required this.hintText,
+    this.isObscureText,
+    this.suffixIcon,
+    this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        isDense: true,
+        contentPadding: contentPadding ??
+            EdgeInsets.symmetric(horizontal: 20.w, vertical: 18.h),
+        focusedBorder: focusedBorder ??
+            OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: AppColors.mainBlue,
+                width: 1.3,
+              ),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+        enabledBorder: enabledBorder ??
+            OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: AppColors.lighterGray,
+                width: 1.3,
+              ),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+        hintStyle: hintStyle ?? TextStyles.font14LightGrayWheight500,
+        hintText: hintText,
+        suffixIcon: suffixIcon,
+        fillColor: backgroundColor ?? AppColors.moreLighterGray,
+        filled: true,
+      ),
+      obscureText: isObscureText ?? false,
+      style: TextStyles.font14DartBlueWheight500,
+    );
+  }
+}
